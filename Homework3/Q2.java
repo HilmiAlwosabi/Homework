ecc5a554549ef464823816186f3ddadcf99a1863
HilmiAlwosabi <111094485+HilmiAlwosabi@users.noreply.github.com>
2022-08-30 19:25:47 +0300

    Update Q2.java

--- Homework3/Q2.java
+++ Homework3/Q2.java
@@ -4,21 +4,21 @@ import java.util.Scanner;
 public class Q2 {
     public static void main(String[] args) {
         Scanner in = new Scanner (System.in);
-        char character[] = new char[] {'a','c','e','g','b','d','f','h'};
+        char letter[] = new char[] {'a','c','e','g','b','d','f','h'};
         
-        System.out.println("Enter the letter and then the number to find the color of the squara on the chessboard.");
+        System.out.println("Enter the letter and then the number.");
         System.out.print("Enter the letter : ");
-        char letter = in.next().charAt(0);
+        char letter1 = in.next().charAt(0);
         System.out.print("Enter the number: ");
-        byte number = in.nextByte();
+        byte num = in.nextByte();
         
         for (int i=0; i<8; i++)
-            if (letter == character[i] && i<4){
-                if (number%2 == 1) { System.out.println("Black"); break;}
+            if (letter1 == letter[i] && i<4){
+                if (num%2 == 1) { System.out.println("Black"); break;}
                 else { System.out.println("White"); break;}
             }
             else {
-                if (number%2 == 1) { System.out.println("White"); break;}
+                if (num%2 == 1) { System.out.println("White"); break;}
                 else { System.out.println("Black"); break;}
             }  
         
